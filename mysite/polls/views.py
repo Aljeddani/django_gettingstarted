3f8ff91c8071d5e6d7ebab5d4541c6c1f6a50fa4
Aljeddani <aljeddani99@gmail.com>
2016-09-02 19:42:50 +0100

    New updates ...

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
+    print('This is what request looks like : {0}'.format(str(request)))
     return HttpResponse('<h1>Hello this is the polls app!!</h1>')
